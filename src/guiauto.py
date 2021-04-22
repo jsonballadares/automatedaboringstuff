ee20ea2af9b2c018988be4d64c537f7531f58a77
jsonballadares <jsonballadares@gmail.com>
2021-04-22 11:46:11 -0400

    guiauto is interesting

---
+++ src/guiauto.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+print(pyautogui.size())
+print(pyautogui.position())
+pyautogui.moveTo(10, 10, duration=1.5)
+pyautogui.moveRel(200, 0, duration=1.5)
+pyautogui.moveRel(0, 200, duration=1.5)
